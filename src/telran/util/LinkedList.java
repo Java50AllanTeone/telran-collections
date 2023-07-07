2a71e6b646e5ec6794e68eca650be619f3921044
allanteone <allanteone@gmail.com>
2023-07-07 09:45:27 +0300

    CW-13

---
+++ src/telran/util/LinkedList.java
@@ -0,0 +1,111 @@
+package telran.util;
+
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.function.Predicate;
+
+public class LinkedList<T> implements List<T> {
+
+	@Override
+	public boolean add(T obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void ensureCapacity(int capacity) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void trimToSize() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getCapacity() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Object[] toArray() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Iterator<T> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int indexOf(Object pattern) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int lastIndexOf(T pattern) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int indexOf(Predicate<T> predicate) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int lastIndexOf(Predicate<T> predicate) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void add(int index, T object) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void set(int index, T object) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void sort(Comparator<T> c) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public T get(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public T remove(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean addAll(int index, Collection<T> collection) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
---
+++ src/telran/util/LinkedListTest.java
@@ -0,0 +1,14 @@
+package telran.util;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class LinkedListTest extends ListTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
